4437046de28adefedbee9e65a88131cdcb3aafcf
AliceCaselli <alicecaselli3@gmail.com>
2023-05-11 17:11:48 +0200

    corretto file .env

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,17 +1,19 @@
 @extends('layouts/main')
 
 @section('content')
-    @foreach ($movies as $movie)
-        
-        <div class="card" style="width: 18rem;">
-            <div class="card-body">
-            <h5 class="card-title">{{$movie->title}}</h5>
-            <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->original_title}}</h6>
-            <h4 class="nation">{{$movie->nationality}}</h4>
-            <h4 class="date">{{$movie->date}}</h4>
-            <h3 class="vote">{{$movie->vote}}</h3>
+    <div class="container">
+        @foreach ($movies as $movie)
+            
+            <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">{{$movie->title}}</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">{{$movie->original_title}}</h6>
+                <h4 class="nation">{{$movie->nationality}}</h4>
+                <h4 class="date">{{$movie->date}}</h4>
+                <h3 class="vote">{{$movie->vote}}</h3>
+                </div>
             </div>
-        </div>
-    
-    @endforeach
+        
+        @endforeach
+    </div>
 @endsection
